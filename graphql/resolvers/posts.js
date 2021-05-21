3c84fb3650aab4bde1244deb30c9a375adb661a9
Giovani Lamastra <giovanilamastra@gmail.com>
2021-05-21 03:40:35 -0300

    add post mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,43 @@
 const Post = require("../../models/Post");
+const checkAuth = require("../../utils/checkAuth");
 
 module.exports = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
+   Query: {
+      async getPosts() {
+         try {
+            const posts = await Post.find().sort({ createdAt: -1 });
+            return posts;
+         } catch (err) {
+            console.log(err);
+         }
+      },
+      async getPost(_, { postId }) {
+         try {
+            const post = await Post.findById(postId);
+            if (post) {
+               return post;
+            } else {
+               throw new Error("Post not found");
+            }
+         } catch (err) {
+            throw new Error(err);
+         }
+      },
+   },
+   Mutation: {
+      async createPost(_, { body }, context) {
+         const user = checkAuth(context);
+
+         const newPost = new Post({
+            body,
+            user: user.id,
+            username: user.username,
+            createdAt: new Date().toISOString(),
+         });
+
+         const post = await newPost.save();
+
+         return post;
+      },
+   },
 };
